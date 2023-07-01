0b1981101f4e6e137090204c2d7fd8325f087c0f
Hodan Ibrahim <hodanibrahim@hodans-air.home>
2023-07-01 20:36:30 +0100

    changes

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -2,6 +2,7 @@
 const { User } = require('../models');
 // import sign token function from auth
 const { signToken } = require('../utils/auth');
+const server = require('/Book-search-engine/server.js');
 
 module.exports = {
   // get a single user by either their id or their username
